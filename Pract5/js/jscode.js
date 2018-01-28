aa6069f25f2ba636fc86ee0b093bca0d30921aaa
acarmonabelmonte <acarmonabelmonte@gmail.com>
2018-01-28 19:31:10 +0100

    Pract5 finalizada

---
+++ Pract5/js/jscode.js
@@ -0,0 +1,66 @@
+function changeColorOver (celda){ 
+	celda.style.backgroundColor="#94F03D" 
+} 
+function changeColorOut(celda){ 
+	celda.style.backgroundColor="#ffffff" 
+}
+
+function mostrarBBDD(celda){
+var elemento = document.getElementById('BBDD');
+if (elemento.style.display == "none") {
+elemento.style.display = "block"
+} else {
+elemento.style.display = "none"
+};
+return true;
+};
+
+function mostrarLLMM(celda){
+var elemento = document.getElementById('LLMM');
+if (elemento.style.display == "none") {
+elemento.style.display = "block"
+} else {
+elemento.style.display = "none"
+};
+return true;
+};
+
+function mostrarProgramación(celda){
+var elemento = document.getElementById('Programación');
+if (elemento.style.display == "none") {
+elemento.style.display = "block"
+} else {
+elemento.style.display = "none"
+};
+return true;
+};
+
+function mostrarED(celda){
+var elemento = document.getElementById('ED');
+if (elemento.style.display == "none") {
+elemento.style.display = "block"
+} else {
+elemento.style.display = "none"
+};
+return true;
+};
+
+function mostrarFOL(celda){
+var elemento = document.getElementById('FOL');
+if (elemento.style.display == "none") {
+elemento.style.display = "block"
+} else {
+elemento.style.display = "none"
+};
+return true;
+};
+
+function mostrarSSII(celda){
+var elemento = document.getElementById('SSII');
+if (elemento.style.display == "none") {
+elemento.style.display = "block"
+} else {
+elemento.style.display = "none"
+};
+return true;
+};
