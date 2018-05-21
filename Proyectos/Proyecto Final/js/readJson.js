9bb7a67ac25335fd9012efa76323a47979062ba7
Adrián <acarmonabelmonte@gmail.com>
2018-05-21 18:14:04 +0200

    añadido footer (no completo)

--- Proyectos/Proyecto Final/js/readJson.js
+++ Proyectos/Proyecto Final/js/readJson.js
@@ -1,8 +1,10 @@
+//Cargar json como objeto
 $.getJSON( "./json/data1.json", function( jsonObject ) {
-  //en aquest punt l'objecte jsonObject correspon al fitxer
+//En este punto el objeto jsonObject corresponde al fichero
   pintar( jsonObject );
 });
 
+// Pinta json en html 
 function pintar(json){
  $.each( json, function( i, noticia ) {
   $("#out").append( "<h1 id='" + i + "'>" + noticia.titular+ "</h1>" + "<p>" + noticia.descripcion + "</p>"  );
