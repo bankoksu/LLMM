db00773137b412d4d26a0e9e49154aafeb24a329
acarmonabelmonte <acarmonabelmonte@gmail.com>
2018-05-21 21:36:04 +0200

    carga de los dos archivos json

--- Proyectos/Proyecto Final/js/readJson.js
+++ Proyectos/Proyecto Final/js/readJson.js
@@ -1,13 +1,20 @@
-//Cargar json como objeto
+//Cargamos el primer json como objeto
 $.getJSON( "./json/data1.json", function( jsonObject ) {
 //En este punto el objeto jsonObject corresponde al fichero
   pintar( jsonObject );
 });
 
-// Pinta json en html 
+//Cargamos el segundo json como objeto
+$.getJSON( "./json/data2.json", function( jsonObject ) {
+//En este punto el objeto jsonObject corresponde al fichero
+  pintar( jsonObject );
+});
+
+// Pintamos los ficheros json en html 
 function pintar(json){
  $.each( json, function( i, noticia ) {
   $("#out").append( "<h1 id='" + i + "'>" + noticia.titular+ "</h1>" + "<p>" + noticia.descripcion + "</p>"  );
  }); 
 }
+
 
