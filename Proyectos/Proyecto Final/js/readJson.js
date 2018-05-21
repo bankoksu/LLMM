d787676e95c994719237c770c826207d46127e0e
acarmonabelmonte <acarmonabelmonte@gmail.com>
2018-05-21 13:47:49 +0200

    mejoras
    añadido bootstrap 4, añadida función js readJSON, añadida barra navegador

--- Proyectos/Proyecto Final/js/readJson.js
+++ Proyectos/Proyecto Final/js/readJson.js
@@ -5,17 +5,7 @@ $.getJSON( "./json/data1.json", function( jsonObject ) {
 
 function pintar(json){
  $.each( json, function( i, noticia ) {
-  $("#out1").append( "<h1 id='" + i + "'>" + noticia.titular+ "</h1>" + "<p>" + noticia.descripcion + "</p>"  );
+  $("#out").append( "<h1 id='" + i + "'>" + noticia.titular+ "</h1>" + "<p>" + noticia.descripcion + "</p>"  );
  }); 
 }
 
-$.getJSON( "./json/data2.json", function( jsonObject ) {
-  //en aquest punt l'objecte jsonObject correspon al fitxer
-  pintar( jsonObject );
-});
-
-function pintar(json){
- $.each( json, function( i, noticia ) {
-  $("#out2").append( "<h1 id='" + i + "'>" + noticia.titular+ "</h1>" + "<p>" + noticia.descripcion + "</p>"  );
- }); 
-}
