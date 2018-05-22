3f56d3575e201f8940badbfe6afe022f9ea4a629
Adrián Carmona <32549529+acarmonabelmonte@users.noreply.github.com>
2018-05-22 10:44:36 +0200

    scroll infinito y carga de json

---
+++ Proyectos/Proyecto Final/js/infinteScroll.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    var win = $(window);
+
+    // Cada vez que el usuario se desplaza
+    win.scroll(function() {
+        // Comprobar si es el final del documento
+        if ($(document).height() - win.height() == win.scrollTop()) {
+            $('#loading').show();
+        //Función para cargar fichero JSON
+            $.getJSON( "./json/data1.json", function( jsonObject ) {
+        //En este punto el objeto jsonObject corresponde al fichero
+          pintar( jsonObject );
+        });
+
+        //Cargamos el segundo json como objeto
+        $.getJSON( "./json/data2.json", function( jsonObject ) {
+        //En este punto el objeto jsonObject corresponde al fichero
+          pintar( jsonObject );
+        });
+
+        // Pintamos los ficheros json en html 
+        function pintar(json){
+         $.each( json, function( i, noticia ) {
+          $("#out").append( "<h1 id='" + i + "'>" + noticia.titular+ "</h1>" + "<p>" + noticia.descripcion + "</p>"  );
+         }); 
+        }
+        }
+    });
+});
