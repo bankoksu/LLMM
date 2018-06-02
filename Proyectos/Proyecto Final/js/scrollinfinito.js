7059e2c02abae88bacb6638b70130d9cfa0ed75c
acarmonabelmonte <acarmonabelmonte@gmail.com>
2018-06-02 22:02:51 +0200

    finalizada página programación

--- Proyectos/Proyecto Final/js/scrollinfinito.js
+++ Proyectos/Proyecto Final/js/scrollinfinito.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
     var win = $(window);
+    var parar = 0;
 
     // Cada vez que el usuario se desplaza
     win.scroll(function() {
         // Comprobar si es el final del documento
-        if ($(document).height() - win.height() == win.scrollTop()) {
+        if ($(document).height() - win.height() == win.scrollTop() && parar == 0) {
             $('#loading').show();
         //Función para cargar fichero JSON
             $.getJSON( "../json/data1.json", function( jsonObject ) {
@@ -14,8 +15,8 @@ $(document).ready(function() {
         // Pintamos los ficheros json en html 
         function pintar(json){
          $.each( json, function( i, noticia ) {
-          $("#out").append( "<h1 id='" + i + "'>" + noticia.titular+ "</h1>" + "<p>" + noticia.descripcion + "</p>"  );
-          win = null;
+          $("#out").append( "<h3 id='" + i + "'>" + noticia.titular+ "</h3>" + "<p>" + "<img class='img-fluid float-left img-thumbnail mr-4' src='" + noticia.imagen + "'>" + noticia.descripcion + "</p>" + "<div class='mb-1 text-muted'>" + noticia.datetime + "</div>");
+          parar = parar + 1;
           $('#loading').hide();
          }); 
         }
